a1e62c35af8c853229fdc3551109367d74b7a3dc
bradbaileymurray@gmail.com <bradbaileymurray@gmail.com>
2018-05-15 22:18:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ return "Happy holidays!";
 
 }
 function happyHolidaysTo(name) {
-return 'Happy holidays, ${name}!'
+return `Happy holidays, name}!`
 
 }
 
 function holidayCountdown(days, holiday) {
-return 'It\'s ${days} days until ${holiday}!';
+return `It\'s ${days} days until ${holiday}!`;
 
 
 }
