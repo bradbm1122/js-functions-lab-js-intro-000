617b7e127fa388204888c6af403c27c07b887380
bradbaileymurray@gmail.com <bradbaileymurray@gmail.com>
2018-05-15 19:39:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 function happyHolidays(){
 return "Happy holidays";
 
-  
+
 }
 function happyHolidaysTo(name){
 return "Happy holidays, ${name}!"
-  
+
 }
 
 function holidayCountdown(days, holiday){
 return "It's ${days} days until ${holiday}!";
-  
+
 }
